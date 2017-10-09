36e6bf2ba0daa543eb743023417a964392419547
Stefano Salati <stef.salati@gmail.com>
2017-10-09 10:25:25 +0200

    PID implemented, needs tuning

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,24 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  p_error_ = 0;
+  i_error_ = 0;
+  d_error_ = 0;
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error_ = cte - p_error_;
+  p_error_ = cte;
+  i_error_ += cte;
 }
 
 double PID::TotalError() {
+  double error = -Kp_ * p_error_
+                 -Ki_ * i_error_
+                 -Kd_ * d_error_;
+  return error;
 }
 
